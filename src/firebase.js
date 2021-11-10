2156c9efcac039be7a46568b8d92594faf524840
bourkison <harrisonbourke97@gmail.com>
2021-11-11 01:49:36 +1100

    Revert "Deleted firebase.js"
    This reverts commit 9c33588a6bbc649b15d86d0bab86fc0a78bd0173.

---
+++ src/firebase.js
@@ -0,0 +1,49 @@
+// import firebase from 'firebase'
+
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// var firebaseConfig = {
+//     apiKey: "AIzaSyD34Lvj0Q-Wm0o9CULDKXucRw_eLSnPnBE",
+//     authDomain: "burn-project-f8493.firebaseapp.com",
+//     projectId: "burn-project-f8493",
+//     storageBucket: "burn-project-f8493.appspot.com",
+//     messagingSenderId: "737450034110",
+//     appId: "1:737450034110:web:c526ade8c43730adf02fef",
+//     measurementId: "G-P5MMTY71BB"
+// };
+// // Initialize Firebase
+// firebase.initializeApp(firebaseConfig);
+// firebase.analytics();
+
+// const db = firebase.firestore();
+// const auth = firebase.auth();
+// const storage = firebase.storage();
+// const fv = firebase.firestore.FieldValue;
+
+// // Collections:
+// const templatesCollection = () => {
+//     return db.collection("workouts");  
+// }
+// const userTemplatesCollection = (userId) => {
+//     return db.collection("users").doc(userId).collection("workouts");
+// }
+// const userWorkoutsCollection = (userId) => {
+//     return db.collection("users").doc(userId).collection("burns")
+// }
+
+// // Set our functions region.
+// var functions = firebase.app().functions("australia-southeast1");
+
+// // Function waits for login to happen, and is called in guarded routes.
+// // Fixes the problem of instantly getting redirected on guarded routes when
+// // firebase hasn't had time to log in yet.
+// firebase.getCurrentUser = () => {
+//     return new Promise((resolve, reject) => {
+//         const unsubscribe = auth.onAuthStateChanged(user => {
+//             unsubscribe();
+//             resolve(user)
+//         }, reject);
+//     })
+// };
+
+// export { db, auth, fv, storage, functions, templatesCollection, userTemplatesCollection, userWorkoutsCollection }
