c3f3bf76a611dd5d50e4c0cccbdfef6327e3caf7
bourkison <harrisonbourke97@gmail.com>
2022-01-11 23:18:05 +1100

    Delete src/.nuxt directory

--- src/.nuxt/middleware.js
+++
@@ -1,6 +0,0 @@
-const middleware = {}
-
-middleware['requiresAuth'] = require('../middleware/requiresAuth.js')
-middleware['requiresAuth'] = middleware['requiresAuth'].default || middleware['requiresAuth']
-
-export default middleware
